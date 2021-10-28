7c5220184c79e30e9f6ebfc3aefd8a8ef430430d
Parminder Singh <parminder.singh629@gmail.com>
2021-10-28 16:33:07 +0530

    set up project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+/**
+ * Home Page of the Application
+ * @return {JSX.Element}
+ */
+function App() {
+  return <div className="App">hello</div>;
+}
+
+export default App;
