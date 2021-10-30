d06601f05fea446eee96f7a05acf9288e82d3ff2
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-10-30 13:02:41 +0530

    Added NumberGenerated functionality and testcases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import NumberGenerator from "./components/NumberGenerator";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("NumberGenerator ", () => {
+  it("should render text 'Current Number' ", () => {
+    render(<NumberGenerator />);
+    const text = screen.getByText(/Current Number/i);
+    expect(text).toBeInTheDocument();
+  });
+  it("should zero be the initial value of current number", () => {
+    render(<NumberGenerator />);
+    const number = screen.getByTestId(/currentNo/i);
+    expect(number).toHaveTextContent("0");
+  });
 });
---
+++ src/components/NumberGenerator.tsx
@@ -0,0 +1,46 @@
+import React, { useCallback, useState } from "react";
+import random from "lodash.random";
+import { Button } from "@mui/material";
+
+interface State {
+  prevNumber: null | number;
+  currentNumber: null | number;
+  pickedNumbers: number[];
+}
+
+const NumberGenerator = () => {
+  const [state, setState] = useState<State>({
+    prevNumber: null,
+    currentNumber: 0,
+    pickedNumbers: [],
+  });
+
+  const generateNumber = useCallback(() => {
+    const randomNumber = random(1, 90);
+    if (state.pickedNumbers.includes(randomNumber)) {
+      generateNumber();
+    } else {
+      setState({
+        prevNumber: state.currentNumber,
+        currentNumber: randomNumber,
+        pickedNumbers: [...state.pickedNumbers, randomNumber],
+      });
+    }
+  }, [state.prevNumber, state.currentNumber]);
+
+  return (
+    <div>
+      <div>
+        <div>Current Number</div>
+        <div>{state.currentNumber}</div>
+      </div>
+      <Button onClick={generateNumber}>Generate Number</Button>
+      <div>
+        <div>Previous Number</div>
+        <div>{state.prevNumber}</div>
+      </div>
+    </div>
+  );
+};
+
+export default NumberGenerator;
