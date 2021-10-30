0081c87fafb71cdc4ae2acec7019206d60a0e636
nik-1207 <nmknikhilkashyap@gmail.com>
2021-10-30 15:17:38 +0530

    adding Board,utility methods and testcase for Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
+import Board from "./components/Board";
 /**
  * Home Page of the Application
  * @return {JSX.Element}
  */
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
 }
 
 export default App;
