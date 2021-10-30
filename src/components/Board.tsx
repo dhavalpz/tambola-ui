7a79b518c497f4cbb2d57dde7772a92b8e7606d7
nik-1207 <nmknikhilkashyap@gmail.com>
2021-10-30 15:39:19 +0530

    refractoring code

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Paper } from "@mui/material";
+import { Grid, Paper, Typography } from "@mui/material";
 import { formatNumber, LIMIT } from "../util/utility";
 
 const Board: React.FC = () => {
@@ -9,21 +9,26 @@ const Board: React.FC = () => {
   );
 
   return (
-    <Grid
-      container
-      columns={10}
-      padding="20px"
-      rowSpacing={2}
-      columnSpacing={2}
-    >
-      {arr.map((num: number, index: number) => (
-        <Grid item key={index} xs={2} md={1}>
-          <Paper sx={{ border: "2px solid red", padding: "5px" }} elevation={3}>
-            {formatNumber(num)}
-          </Paper>
-        </Grid>
-      ))}
-    </Grid>
+    <Paper>
+      <Grid
+        container
+        columns={10}
+        padding="20px"
+        rowSpacing={2}
+        columnSpacing={2}
+      >
+        {arr.map((num: number, index: number) => (
+          <Grid item key={index} xs={2} md={1}>
+            <Paper
+              sx={{ border: "2px solid red", padding: "5px" }}
+              elevation={3}
+            >
+              <Typography align="center">{formatNumber(num)}</Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Paper>
   );
 };
 
