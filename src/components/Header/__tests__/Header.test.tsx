7b14862c9ec4dda9c64c97701105c312b0013e66
nik-1207 <nmknikhilkashyap@gmail.com>
2021-11-01 11:54:17 +0530

    refractoring code

--- src/components/Header/__tests__/Header.test.tsx
+++ src/components/Header/__tests__/Header.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Header } from "../Header";
+import Header from "../Header";
 import { fireEvent, render, screen } from "@testing-library/react";
 import { ThemeContext } from "../../ThemeContext";
 
