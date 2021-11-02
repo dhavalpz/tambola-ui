ef8f008a38ccc8a0525870cacd9c849351a154bc
Parminder Singh <parminder.singh629@gmail.com>
2021-11-02 17:00:24 +0530

    conflict resolved

--- src/components/NumberGenerator/NumberGenerator.test.tsx
+++ src/components/NumberGenerator/NumberGenerator.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
-import NumberGenerator from "../components/NumberGenerator";
+import NumberGenerator from "./NumberGenerator";
 
 jest.mock("lodash", () => {
   return {
--- src/components/NumberGenerator/NumberGenerator.tsx
+++ src/components/NumberGenerator/NumberGenerator.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from "react";
 import { random } from "lodash";
 import { Button } from "@mui/material";
-import { START_VALUE, END_VALUE } from "../util/utility";
+import { START_VALUE, END_VALUE } from "../../util/utility";
 interface State {
   prevNumber: null | number;
   currentNumber: null | number;
